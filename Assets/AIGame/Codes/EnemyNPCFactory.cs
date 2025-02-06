fbf6b0ed816b9d431c37303306cde51b0f1c1b64
CDiegoSV <cdsalgadov@gmail.com>
2025-02-05 18:48:33 -0600

    EnemyNPC SO Parameters Update
    Added parameters like FOV, distance, position, and more.

--- Assets/AIGame/Codes/EnemyNPCFactory.cs
+++ Assets/AIGame/Codes/EnemyNPCFactory.cs
@@ -1,4 +1,3 @@
-using NUnit.Framework;
 using UnityEngine;
 using System.Collections.Generic;
 
@@ -10,16 +9,20 @@ namespace Dante.Agents
         #region References
         [Header("Parameters")]
         [SerializeField] protected GameObject enemyPrefab;
+        [SerializeField] protected GameObject visionConePrefab;
         [SerializeField] protected EnemyNPC_SO[] enemiesScriptableObject;
 
         [Header("Runtime Variables")]
         [SerializeField] protected List<GameObject> enemyInstancesGameObjects;
-
+        
         #endregion
 
         #region RuntimeVariables
 
-        GameObject enemyInstanceGameObject;
+        protected GameObject _enemyInstanceGameObject;
+
+        protected GameObject _currentVisionCone;
+        [SerializeField] protected List<GameObject> _currentEnemyVisionCones;
 
         #endregion
 
@@ -30,26 +33,70 @@ namespace Dante.Agents
             DeleteEnemies();
             foreach (EnemyNPC_SO enemy in enemiesScriptableObject)
             {
-                enemyInstanceGameObject = Instantiate(enemyPrefab);
-                enemyInstanceGameObject.transform.position = enemy.spawnParameters.position;
-                enemyInstanceGameObject.transform.rotation = Quaternion.Euler(enemy.spawnParameters.rotation);
-                enemyInstanceGameObject.transform.parent = this.gameObject.transform;
+                _enemyInstanceGameObject = Instantiate(enemyPrefab, this.gameObject.transform);
+                _enemyInstanceGameObject.transform.position = enemy.spawnParameters.position;
+                _enemyInstanceGameObject.transform.rotation = Quaternion.Euler(enemy.spawnParameters.rotation);
 
-                enemyInstancesGameObjects.Add(enemyInstanceGameObject);
+                enemyInstancesGameObjects.Add(_enemyInstanceGameObject);
+
+                CreateVisionCones(enemy);
             }
         }
 
         public void DeleteEnemies()
         {
-            for(int i = enemyInstancesGameObjects.Count -1; i >= 0; i--)
+            if (enemyInstancesGameObjects.Count > 0)
             {
-                enemyInstanceGameObject = enemyInstancesGameObjects[i];
-                enemyInstancesGameObjects.Remove(enemyInstanceGameObject);
-                DestroyImmediate(enemyInstanceGameObject);
+                for (int i = enemyInstancesGameObjects.Count - 1; i >= 0; i--)
+                {
+                    _enemyInstanceGameObject = enemyInstancesGameObjects[i];
+                    enemyInstancesGameObjects.Remove(_enemyInstanceGameObject);
+                    DestroyImmediate(_enemyInstanceGameObject);
+                }
+                enemyInstancesGameObjects.Clear();
             }
-            enemyInstancesGameObjects.Clear();
         }
 
+        public void CreateVisionCones(EnemyNPC_SO enemyNPC_SO)
+        {
+            if(enemyNPC_SO.visionConeParameters.fieldOfView % 10 == 0)
+            {
+                for (int i = 0; i < (enemyNPC_SO.visionConeParameters.fieldOfView * 0.1f) -1; i++)
+                {
+                    _currentVisionCone = Instantiate(visionConePrefab, _enemyInstanceGameObject.transform.GetChild(2));
+                    _currentEnemyVisionCones.Add(_currentVisionCone);
+                }
+                int signIterator = 1;
+                int degrees = 0;
+
+                for (int j = 0; j < _currentEnemyVisionCones.Count; j++)
+                {
+
+                    _currentEnemyVisionCones[j].transform.localScale = new Vector3(enemyNPC_SO.visionConeParameters.distance, 1, enemyNPC_SO.visionConeParameters.distance);
+
+                    _currentEnemyVisionCones[j].transform.localRotation = Quaternion.Euler(0, degrees * signIterator, 0);
+
+                    if (j > 0)
+                    {
+                        signIterator *= -1;
+                    }
+                    if (signIterator > 0)
+                    {
+                        degrees += 10;
+                    }
+                }
+                if (_currentEnemyVisionCones.Count % 2 == 0)
+                {
+                    _enemyInstanceGameObject.transform.GetChild(2).localRotation = Quaternion.Euler(new Vector3(0, -5, 0));
+                }
+                _currentEnemyVisionCones.Clear();
+                _currentEnemyVisionCones = new List<GameObject>();
+            }
+            else
+            {
+                Debug.LogWarning("CreateVisionCones() - Field of View of " + enemyNPC_SO.name + " is not a multiple of 10.");
+            }
+        }
         #endregion
     }
 }
